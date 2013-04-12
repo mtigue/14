8e2d37fc6c81dc6a83a6f335b5b67f3aced3d5b0
Michael Tigue <mtigue@web4uonline.com>
2013-04-12 08:42:34 -0400

    1

---
+++ assets/images/splashscreens/_vti_cnf/apple-touch-startup-image-768x1004.png
@@ -0,0 +1,4 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|11 Apr 2013 23:34:36 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|_codes/index.html 13/index.html
---
+++ assets/js/_vti_cnf/add2home.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|11 Apr 2013 23:34:36 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|_codes/index.html 13/index.html
+vti_author:SR|web4u\\mtigue
+vti_modifiedby:SR|web4u\\mtigue
+vti_timecreated:TR|11 Apr 2013 23:34:36 -0000
+vti_cacheddtm:TX|11 Apr 2013 23:34:36 -0000
+vti_filesize:IR|13764
