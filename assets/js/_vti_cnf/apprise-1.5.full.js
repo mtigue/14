c539b84cb917f7d88c691347b356c9a49cf6f8d6
Michael Tigue <mtigue@web4uonline.com>
2013-06-23 18:38:15 -0400

    40

---
+++ assets/js/_vti_cnf/apprise-1.5.full.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|web4u\\mtigue
+vti_modifiedby:SR|web4u\\mtigue
+vti_timelastmodified:TR|23 Jun 2013 19:02:52 -0000
+vti_timecreated:TR|23 Jun 2013 19:02:13 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|14/main.html
+vti_nexttolasttimemodified:TR|23 Jun 2013 19:02:23 -0000
+vti_cacheddtm:TX|23 Jun 2013 19:02:52 -0000
+vti_filesize:IR|3550
